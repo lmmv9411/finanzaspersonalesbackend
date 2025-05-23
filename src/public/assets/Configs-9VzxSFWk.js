9310373078966e75faa69a28295f34f48f874cb7
lmmv9411 <luismmv9411@gmail.com>
2025-05-23 07:38:00 -0500

    front end

--- src/public/assets/Configs-9VzxSFWk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-5brMnQfe.js";const o={};function r(s,t){return n(),c("h1",null,"Configuraciones")}const _=e(o,[["render",r]]);export{_ as default};
--- src/public/assets/categoriesStore-D_L3auj5.js
+++
@@ -1,1 +0,0 @@
-import{A as t,r as o,x as a}from"./index-5brMnQfe.js";const i=t("categories",()=>{const r=o([]);return{categories:r,getCategories:async()=>{try{const e=await a.get("/categories");r.value=e.data}catch(e){console.error("Error fetching categories:",e)}}}});export{i as u};
