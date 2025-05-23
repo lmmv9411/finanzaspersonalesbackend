9045622ca2f70338fb62b0293d1f5e6c396328c8
lmmv9411 <luismmv9411@gmail.com>
2025-05-23 07:44:07 -0500

    ajustes icon

---
+++ src/public/assets/categoriesStore-CfWA82E7.js
@@ -0,0 +1,1 @@
+import{A as t,r as o,x as a}from"./index-C2PL0gPw.js";const i=t("categories",()=>{const r=o([]);return{categories:r,getCategories:async()=>{try{const e=await a.get("/categories");r.value=e.data}catch(e){console.error("Error fetching categories:",e)}}}});export{i as u};
