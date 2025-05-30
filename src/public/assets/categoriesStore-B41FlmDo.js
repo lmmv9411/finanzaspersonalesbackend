66e93553e42f9a77443ebedb51e46e73faae7913
lmmv9411 <luismmv9411@gmail.com>
2025-05-29 22:33:22 -0500

    categories

--- src/public/assets/categoriesStore-B41FlmDo.js
+++
@@ -1,1 +0,0 @@
-import{z as t,r as o,m as a}from"./index-CNFUwRfF.js";const i=t("categories",()=>{const r=o([]);return{categories:r,getCategories:async()=>{try{const e=await a.get("/categories");r.value=e.data}catch(e){console.error("Error fetching categories:",e)}}}});export{i as u};
