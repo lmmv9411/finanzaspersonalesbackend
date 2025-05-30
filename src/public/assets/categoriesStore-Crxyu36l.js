af06b0c05add1cc40d4af493316c0384b3f1b294
lmmv9411 <luismmv9411@gmail.com>
2025-05-30 10:52:50 -0500

    frontend

---
+++ src/public/assets/categoriesStore-Crxyu36l.js
@@ -0,0 +1,1 @@
+import{z as t,r as o,m as a}from"./index-6yIVpIIz.js";const i=t("categories",()=>{const r=o([]);return{categories:r,getCategories:async()=>{try{const e=await a.get("/categories");r.value=e.data}catch(e){console.error("Error fetching categories:",e)}}}});export{i as u};
