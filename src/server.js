386bd3026e14026339ca6994327c4b82d3aa2091
lmmv9411 <luismmv9411@gmail.com>
2025-06-03 12:07:53 -0500

    fix

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ app.use(cookieParser())
 
 const _dirname = path.dirname(fileURLToPath(import.meta.url))
 
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')))
+app.use('/uploads', express.static(path.join(_dirname, 'public', 'uploads')))
 app.use(express.static(path.join(_dirname, 'public')))
 
 app.use('/api/movements', movementRoutes)
