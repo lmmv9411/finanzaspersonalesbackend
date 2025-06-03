9aeef9c500ab2a925a6e624938b391d148ab1e2a
lmmv9411 <luismmv9411@gmail.com>
2025-06-03 11:53:48 -0500

    ajuste

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ app.use(cookieParser())
 const _dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(path.join(_dirname, 'public')))
-app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
+app.use('/uploads', express.static(path.join(_dirname, '../public/uploads')));
 
 app.use('/api/movements', movementRoutes)
 app.use('/api/categories', categoryRoutes)
