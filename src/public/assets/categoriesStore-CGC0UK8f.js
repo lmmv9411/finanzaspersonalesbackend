0dadcb4d6df7800794f86e7aa03dbec683a96f83
lmmv9411 <luismmv9411@gmail.com>
2025-06-29 08:53:53 -0500

    ajustes fecha frontend

--- src/public/assets/categoriesStore-CGC0UK8f.js
+++
@@ -1,1 +0,0 @@
-import{P as t,h as o,j as a}from"./index-Dgk1dbwt.js";const i=t("categories",()=>{const r=o([]);return{categories:r,getCategories:async()=>{try{const e=await a.get("/categories");r.value=e.data}catch(e){console.error("Error fetching categories:",e)}}}});export{i as u};
