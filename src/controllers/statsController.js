2c22611e9a7ac9b331a037a22a373c6c019f6556
lmmv9411 <luismmv9411@gmail.com>
2025-07-03 17:50:26 -0500

    stats

--- src/controllers/statsController.js
+++ src/controllers/statsController.js
@@ -1,7 +1,7 @@
 import { Op } from "sequelize"
 import { Category } from "../models/category.js"
-import { Movement } from "../models/movement.js"
 import { sequelize } from "../models/index.js"
+import { Movement } from "../models/movement.js"
 
 export const getTotalByCategory = async (req, res) => {
     try {
@@ -54,4 +54,75 @@ export const getTotalByCategory = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
+}
+
+export const getMonthly = async (req, res) => {
+
+    const UserId = req.user.id;
+
+    const currentYear = new Date().getFullYear();
+    const startDate = new Date(`${currentYear}-01-01`);
+
+    try {
+        const [ingresos, gastos] = await Promise.all([
+            Movement.findAll({
+                attributes: [
+                    [sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m'), 'month'],
+                    [sequelize.fn('SUM', sequelize.col('amount')), 'total']
+                ],
+                where: {
+                    UserId,
+                    type: 'ingreso',
+                    date: { [Op.gte]: startDate }
+                },
+                group: ['month'],
+                order: [[sequelize.literal('month'), 'ASC']],
+
+            }),
+            Movement.findAll({
+                attributes: [
+                    [sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m'), 'month'],
+                    [sequelize.fn('SUM', sequelize.col('amount')), 'total']
+                ],
+                where: {
+                    UserId,
+                    type: 'gasto',
+                    date: { [Op.gte]: startDate }
+                },
+                group: ['month'],
+                order: [[sequelize.literal('month'), 'ASC']],
+
+            })
+        ])
+
+        const result = {}
+
+        ingresos.forEach(ingreso => {
+            const month = ingreso.get('month')
+            if (!result[month]) {
+                result[month] = { ingreso: 0, gasto: 0, saldo: 0 }
+            }
+            result[month].ingreso = parseFloat(ingreso.get('total')) || 0
+        })
+
+        gastos.forEach(gasto => {
+            const month = gasto.get('month')
+            if (!result[month]) {
+                result[month] = { ingreso: 0, gasto: 0, saldo: 0 }
+            }
+            result[month].gasto = parseFloat(gasto.get('total')) || 0
+        })
+
+
+        Object.keys(result).forEach(month => {
+            result[month].saldo = result[month].ingreso - result[month].gasto;
+        });
+
+        return res.json(result)
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: error.message })
+    }
+
 }
--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { getTotalByCategory } from '../controllers/statsController.js'
+import { getTotalByCategory, getMonthly } from '../controllers/statsController.js'
 import { auth } from '../middlewares/auth.js'
 
 const router = express.Router()
@@ -8,5 +8,6 @@ const router = express.Router()
 router.use(auth)
 
 router.get('/totalByCategory', getTotalByCategory)
+router.get('/monthly', getMonthly)
 
 export default router
