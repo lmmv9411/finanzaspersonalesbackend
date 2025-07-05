5d2a6de1dfe2ce0e743c709996a6aa2f509b7a4e
lmmv9411 <luismmv9411@gmail.com>
2025-07-05 10:02:58 -0500

    feat: add movement store and date utility functions
    - Implemented a new movement store component for managing movements with categories.
    - Added a category selection feature with dynamic filtering based on type.
    - Created a date utility function to handle month selection and date formatting.

--- src/public/assets/categoriesStore-CokKkf2a.js
+++
@@ -1,1 +0,0 @@
-import{l as t,h as o,j as a}from"./index-DxG2fk3a.js";const i=t("categories",()=>{const r=o([]);return{categories:r,getCategories:async()=>{try{const e=await a.get("/categories");r.value=e.data}catch(e){console.error("Error fetching categories:",e)}}}});export{i as u};
